062fc1545fb9685202fa9c06ff80712c0d155ed3
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2022-08-23 18:09:24 +1000

    Change path to the favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,7 @@
   <title>@yield('title') | Идём в кино</title>
 
   <!-- Favicon -->
-  <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
   <!-- Scripts -->
   @yield('script')
